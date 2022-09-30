898729b9b26072fe8bd7fc0915c645e11d88e032
pavbyte <pavbyte@gmail.com>
2022-10-01 00:05:05 +0530

    data types in java

---
+++ 1 Data Types/DataTypes.java
@@ -0,0 +1,17 @@
+package Intro;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        /*
+         * primitive and non - primitive
+         */
+
+        // primitive byte,short,int,long,float,double,boolean,char
+        // non-primitive class,string,array
+
+        byte first = 9; // will store 8 bits of data
+        short second = 2344;
+
+        System.out.println(first);
+    }
+}
