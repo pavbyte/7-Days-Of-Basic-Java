0bce7d5e284e0fd16511f1fa4cbdc4b0ec3774fb
pavbyte <pavbyte@gmail.com>
2022-10-01 00:04:38 +0530

    Hello World and comments in Java

---
+++ 0 Hello World/CommentInJava.java
@@ -0,0 +1,11 @@
+package Intro;
+
+public class CommentInJava {
+    public static void main(String[] args) {
+        // two types of comment line in java - first type - single line comment
+        /*
+         * - second type - double line comment
+         */
+
+    }
+}
---
+++ 0 Hello World/HelloWorld.java
@@ -0,0 +1,12 @@
+package Intro;
+//this is the basic java programs
+
+class HelloWorld {
+    public static void main(String[] args) {
+        // public is a access modifier
+        // static means object cannot be created for statically defined methos
+        // void - no return expected or type
+        System.out.println("Hello world");
+        // inbuilt function to print something on screen
+    }
+}
